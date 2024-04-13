75868c5f3dc87916b03005b24d5f96475c51513b
logan <gnlee95@blocko.io>
2024-04-14 01:37:22 +0900

    chore: MariaDB Connection 설정

---
+++ src/main/kotlin/com/ohforbidden/bugreport/infrastructure/config/JpaConfig.kt
@@ -0,0 +1,8 @@
+package com.ohforbidden.bugreport.infrastructure.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
+@Configuration
+@EnableJpaAuditing
+class JpaConfig
